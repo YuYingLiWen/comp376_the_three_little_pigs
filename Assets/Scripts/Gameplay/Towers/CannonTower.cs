371f69ef87b325d307cc96ed481905db490ad13e
Yu Ying Li Wen <usbthekey@gmail.com>
2023-11-08 19:47:37 -0500

    cannon scirpts and other assets

--- Assets/Scripts/Gameplay/Towers/CannonTower.cs
+++ Assets/Scripts/Gameplay/Towers/CannonTower.cs
@@ -1,6 +1,19 @@
-﻿
+﻿using UnityEngine;
+
 public sealed class CannonTower : Towers
 {
+    [SerializeField] GameObject projectile;
+
+    protected override void Fire()
+    {
+        base.Fire();
+
+        var obj = Instantiate(projectile);
+        obj.transform.parent = transform;
+        obj.transform.position = transform.position;
+        obj.transform.up = target.position - transform.position;
+    }
+
     public override void Upgrade()
     {
         base.Upgrade();
